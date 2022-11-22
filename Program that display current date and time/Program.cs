85511efced3a99362ee31f74af95d6b42d0216c8
Okafornta <hillaryonia@gmail.com>
2022-11-22 08:57:55 +0100

    Add project files.

---
+++ Program that display current date and time/Program.cs
@@ -0,0 +1,19 @@
+﻿// TASK: writing a program that displays the current date and time to the user.
+
+// Variable of DateTime type, at first empty
+DateTime now;
+DateTime today;
+
+// Storing of current date and time into our variable
+now = DateTime.Now;
+today = DateTime.Today;
+
+// Output
+Console.WriteLine("Now is " + now);
+
+Console.WriteLine("Today is: " + today);
+
+// Waiting for Enter
+Console.ReadLine();
+
+
